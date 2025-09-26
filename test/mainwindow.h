739a2a4990db3f6cb569fba572f01bfb971a5460
ZUOBIE <dexumeng@seeyond.com.cn>
2025-09-26 21:03:07 +0800

    添加第一个工程

---
+++ test/mainwindow.h
@@ -0,0 +1,26 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include "jkqtplotter/graphs/jkqtplines.h"
+#include "jkqtplotter/jkqtplotter.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    JKQTPlotter *plot;   // 成员指针，生命周期跟随 MainWindow
+};
+#endif // MAINWINDOW_H
