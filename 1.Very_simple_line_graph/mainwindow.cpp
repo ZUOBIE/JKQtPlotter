de57d080ff88b291ae853698561595a5e6b7eda6
ZUOBIE <dexumeng@seeyond.com.cn>
2025-09-28 20:38:48 +0800

    熟练

--- 1.Very_simple_line_graph/mainwindow.cpp
+++ 1.Very_simple_line_graph/mainwindow.cpp
@@ -7,36 +7,19 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
-    // 创建绘图窗口并嵌入主界面
-    plot = new JKQTPlotter(this);//创建"绘图器"并构建链接
-    setCentralWidget(plot);//让绘图器占满主窗口中间区域
-
-    // 获取内部数据存储指针，后续添加数据使用
-    // 库内部有个“Excel 表”式的数据仓库；
-    // 拿它的指针  ds  → 后面只要把数据“丢进仓库”，任何图都能复用同一批数。
-    JKQTPDatastore* ds = plot->getDatastore();
-
-    // 准备正弦曲线数据
-    QVector<double> X, Y;//坐标点向量
-    const int Ndata = 100;//最大数值
-    for (int i = 0; i < Ndata; i++) {
-        double x = double(i) / double(Ndata) * 8.0 * M_PI;//归一化数据到8pi
-        X << x;//等价于push_back，qt的写法
-        Y << sin(x);
+    QVector<double> X,Y;
+    for(int x=-20;x<20;x++)
+    {
+        X << x;
+        Y << 2*pow(x,2) - 4*x + 1;
     }
 
-    // 将数据复制到内部存储，返回列索引
-    size_t columnX = ds->addCopiedColumn(X, "x");
-    size_t columnY = ds->addCopiedColumn(Y, "y");
-
-    // 创建线图对象并绑定数据列
-    JKQTPXYLineGraph* graph1 = new JKQTPXYLineGraph(plot);
-    graph1->setXColumn(columnX);
-    graph1->setYColumn(columnY);
-    graph1->setTitle(QObject::tr("sine graph"));
-
-    // // 把图加入绘图器并自动缩放
-    plot->addGraph(graph1);
+    plot = new JKQTPlotter(this);
+    setCentralWidget(plot);
+    JKQTPXYLineGraph* ga =  new JKQTPXYLineGraph(plot);
+    JKQTPDatastore* pl = plot->getDatastore();
+    ga->setXYColumns(pl->addCopiedColumn(X,"x"),pl->addCopiedColumn(Y,"y"));
+    plot->addGraph(ga);
     plot->zoomToFit();
 }
 
--- 1.Very_simple_line_graph/mainwindow.h
+++ 1.Very_simple_line_graph/mainwindow.h
@@ -4,7 +4,6 @@
 #include <QMainWindow>
 #include "jkqtplotter/graphs/jkqtplines.h"
 #include "jkqtplotter/jkqtplotter.h"
-
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class MainWindow;
